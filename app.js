5c5908acf9c6a8d7ad2daaf4f8ac180c9a401457
Lucas Chinarelli <chinarelli@Chinarelli.local>
2020-04-05 14:30:29 -0300

    Correções e alinhamento dos diretórios do projeto

--- app.js
+++ app.js
@@ -9,15 +9,19 @@ const port = process.env.PORT || 3300;
 
 const app = express();
 
+const session = require('express-session');
+
 // view engine setup
 //app.set('public', path.resolve(__dirname));
 app.set('views', path.resolve(__dirname, 'app', 'views'));
-app.use(express.static(path.resolve(__dirname, 'app', 'views')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.disable('x-powered-by');
 
 app.listen(port, () => {
     console.log(`Front-end is running at localhost:${port}`)
--- public/js/main.js
+++ public/js/main.js
@@ -292,21 +292,26 @@ jQuery(document).ready(function($) {
 	};
 	siteSticky();
 
-	// navigation
+	// Efeito Menu
+	function menuEffect(hash){
+		$('html, body').animate({
+		  'scrollTop': $(hash).offset().top
+		}, 800, 'easeInOutCirc', function(){
+		  window.location.hash = hash;
+		});
+	}
+
+  // navigation
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-      e.preventDefault();
-
-      var hash = this.hash;
-
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 800, 'easeInOutCirc', function(){
-        window.location.hash = hash;
-      });
-
-    });
+	  e.preventDefault();
+	  menuEffect(this.hash);
+	});
+	$("body").on("click", ".main-menu a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
+		e.preventDefault();
+		menuEffect(this.hash);
+	});
   };
   OnePageNavigation();
 
